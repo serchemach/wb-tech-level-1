35ee8409c44d4ea1375250310f806721dbc9ff48
serchemach <serchemach@gmail.com>
2024-09-17 21:29:56 +0300

    Add a draft of task 8

---
+++ task8.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+)
+
+func replaceAtIndex(in string, r rune, i int) string {
+	out := []rune(in)
+	out[i] = r
+	return string(out)
+}
+
+func main() {
+	var (
+		bitInd int
+		bitVal int
+		number int64
+	)
+	flag.IntVar(&bitInd, "index", 0, "Index of the bit to set")
+	flag.IntVar(&bitVal, "value", 1, "New value of the bit")
+	flag.Int64Var(&number, "number", 1213, "The initial number")
+	flag.Parse()
+
+	if bitInd > 63 || bitInd < 0 {
+		log.Fatal("The bit index is out of range [0..63]")
+	}
+
+	if bitVal != 1 && bitVal != 0 {
+		log.Fatal("The bit value is out of range [0,1]")
+	}
+
+	fmt.Printf("The initial number is: %d, or in bit form:\n", number)
+	numberStr := fmt.Sprintf("%b", number)
+	fmt.Println(numberStr)
+
+	arrowString := strings.Repeat(" ", len(numberStr))
+	arrowString = replaceAtIndex(arrowString, '^', len(numberStr)-1-bitInd)
+	fmt.Println(arrowString)
+
+	previousBit := number & (1 << bitInd)
+	fmt.Printf("Bit mask: %b\n", previousBit)
+	fmt.Printf("Bit add mask: %b\n", int64(bitVal)<<int64(bitInd))
+	number = previousBit ^ number ^ (int64(bitVal) << int64(bitInd))
+	fmt.Printf("Number after setting the bit is: %d, or in bit form:\n", number)
+	fmt.Printf("%b\n", number)
+	fmt.Println(arrowString)
+}
