5d0de60a6f272c55252996711bcac12f1934c7ce
serchemach <serchemach@gmail.com>
2024-09-17 17:38:04 +0300

    Add a draft of task 5

--- task5.go
+++ task5.go
@@ -1,14 +1,47 @@
 package main
 
-import "time"
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
 func main() {
 	var timeToLive int
 	flag.IntVar(&timeToLive, "n", 10, "The lifetime of the program in seconds")
 	flag.Parse()
 
-	go func(){
-		time.Sleep(timeToLive * time.Second)
+	var isEnd atomic.Bool
+	go func() {
+		time.Sleep(time.Duration(timeToLive) * time.Second)
+		isEnd.Store(true)
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	valChannel := make(chan int)
+	go func() {
+		for {
+			val, isOpen := <-valChannel
+			if !isOpen {
+				fmt.Println("The end of consumer")
+				wg.Done()
+				return
+			}
+
+			fmt.Printf("Recieved value:%d\n", val)
+		}
+	}()
+
+	for !isEnd.Load() {
+		curVal := rand.Intn(2000)
+		valChannel <- curVal
 	}
 
+	close(valChannel)
+	wg.Wait()
+	fmt.Println("The end of main")
 }
