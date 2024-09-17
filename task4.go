1b96f4829b67d824173b302dfe6588966c553190
serchemach <serchemach@gmail.com>
2024-09-17 17:16:37 +0300

    Add a rough draft of task 4

--- task4.go
+++ task4.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 func main() {
@@ -10,5 +15,42 @@ func main() {
 	flag.IntVar(&numberOfWorkers, "n", 10, "The number of workers")
 	flag.Parse()
 
-	fmt.Println(numberOfWorkers)
+	valChannel := make(chan int)
+	var wg sync.WaitGroup
+
+	for i := range numberOfWorkers {
+		wg.Add(1)
+		go func(workerNum int) {
+			for {
+				val, isOpen := <-valChannel
+				if !isOpen {
+					fmt.Printf("CLOSED!!!!! WORKER #%d\n", workerNum)
+					wg.Done()
+					return
+				}
+				fmt.Printf("WORKER #%d: %d\n", workerNum, val)
+			}
+		}(i + 1)
+	}
+
+	sigChannel := make(chan os.Signal)
+	signal.Notify(sigChannel, os.Interrupt, syscall.SIGTERM)
+	isEnd := false
+
+	go func() {
+		<-sigChannel
+		isEnd = true
+	}()
+
+	for !isEnd {
+		newVal := rand.Intn(2000)
+		valChannel <- newVal
+	}
+
+	fmt.Println("We got a sigterm")
+	close(valChannel)
+
+	// Wait until all goroutines are closed
+	wg.Wait()
+	fmt.Println("We ended stuff")
 }
